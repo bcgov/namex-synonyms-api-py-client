c66d813eb0d9093c1e9dc18e48ca5be979ab4a1a
Lucas <tech@firebrandwebsolutions.com>
2020-04-07 13:20:01 -0700

    Update client.

--- swagger_client/configuration.py
+++ swagger_client/configuration.py
@@ -40,7 +40,7 @@ class Configuration(object):
             return
 
         # Default Base url
-        self.host = "https://localhost/api/v1"
+        self.host = "https://virtserver.swaggerhub.com/lucaslopatka/bcgov-namex-synonyms-api/1.0.0"
         # Temp file folder for downloading files
         self.temp_folder_path = None
 
