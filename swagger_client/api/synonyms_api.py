5458302a01f41516940c71ef413c704a1ebea10f
areyeslo <areyeslo@users.noreply.github.com>
2020-04-29 12:16:59 -0700

    Include response for regex-prefixes.

--- swagger_client/api/synonyms_api.py
+++ swagger_client/api/synonyms_api.py
@@ -1758,7 +1758,7 @@ class SynonymsApi(object):
         :param async_req bool
         :param str text: 
         :param str prefix_list: 
-        :return: SynonymList
+        :return: String
                  If the method is called asynchronously,
                  returns the request thread.
         """
@@ -1780,7 +1780,7 @@ class SynonymsApi(object):
         :param async_req bool
         :param str text: 
         :param str prefix_list: 
-        :return: SynonymList
+        :return: String
                  If the method is called asynchronously,
                  returns the request thread.
         """
@@ -1836,7 +1836,7 @@ class SynonymsApi(object):
             body=body_params,
             post_params=form_params,
             files=local_var_files,
-            response_type='SynonymList',  # noqa: E501
+            response_type='String',  # noqa: E501
             auth_settings=auth_settings,
             async_req=params.get('async_req'),
             _return_http_data_only=params.get('_return_http_data_only'),
